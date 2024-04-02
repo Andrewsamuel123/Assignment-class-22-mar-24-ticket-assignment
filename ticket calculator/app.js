97b5cb0be8546926eea49360c5162ba5f940a8ff
Andrewsamuel123 <163416745+Andrewsamuel123@users.noreply.github.com>
2024-04-02 12:57:38 +0500

    Add files via upload

---
+++ ticket calculator/app.js
@@ -0,0 +1,19 @@
+//let username :string = "bilal"
+//let passward :string = "123"
+//if(username == "bilal" && passward == "123"){
+//  console.log("welcome buddy" +" "+ username)
+//}else{
+// console.log("bye")
+//}
+var priceofticketsforoldage = "500";
+var ageofold = "0";
+var ageofadult = "2";
+var priceofticketsforadult = "300";
+if (ageofadult == "25" && priceofticketsforadult == "300") {
+    console.log("welcome young boy");
+}
+else if (ageofold == "50" && priceofticketsforoldage == '500')
+    console.log("welcome senior citizen");
+else {
+    console.log("fligts are not available for kids ");
+}
---
+++ ticket calculator/app.ts
@@ -0,0 +1,25 @@
+//let username :string = "bilal"
+//let passward :string = "123"
+
+//if(username == "bilal" && passward == "123"){
+  //  console.log("welcome buddy" +" "+ username)
+//}else{
+   // console.log("bye")
+//}
+
+let priceofticketsforoldage :string = "500"
+let ageofold :string = "50"
+let ageofadult:string = "25"
+let priceofticketsforadult :string ="300"
+
+
+
+if(ageofadult == "25" && priceofticketsforadult == "300" ){
+    console.log("welcome young boy")
+    
+}
+else if(ageofold == "50" && priceofticketsforoldage == '500')
+    console.log("welcome senior citizen")
+else{
+    console.log("fligts are not available for kids ")
+}
